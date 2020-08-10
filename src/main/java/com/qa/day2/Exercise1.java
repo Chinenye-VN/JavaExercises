8a183b208f14ccc20330c7fc158cb8689ed17f01
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-10 16:29:05 +0100

    creating an subtraction calculation

--- src/main/java/com/qa/day2/Exercise1.java
+++ src/main/java/com/qa/day2/Exercise1.java
@@ -3,13 +3,21 @@ package com.qa.day2;
 public class Exercise1 {
 
     public static void main(String[] args) {
-        addingIntegers(2,4);
+        addingIntegers(4,2);
+        subtractIntegers(4,2);
     }
 
 
     public static int addingIntegers (int num1 , int num2) {
 
         int result = num1 + num2;
+        System.out.println(result);
+        return result;
+    }
+
+    public static int subtractIntegers (int num1 , int num2) {
+
+        int result = num1 - num2;
         System.out.println(result);
         return result;
     }
