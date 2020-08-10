2bfe4ca806548b26a8a6e8f859e381ecdcbd96f1
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-10 16:35:25 +0100

    creating an multiplication calculation

--- src/main/java/com/qa/day2/Exercise1.java
+++ src/main/java/com/qa/day2/Exercise1.java
@@ -5,6 +5,7 @@ public class Exercise1 {
     public static void main(String[] args) {
         addingIntegers(4,2);
         subtractIntegers(4,2);
+        multiplyIntegers(4,2);
     }
 
 
@@ -20,5 +21,14 @@ public class Exercise1 {
         int result = num1 - num2;
         System.out.println(result);
         return result;
+    }
+    public static int multiplyIntegers (int num1 , int num2) {
+
+        int result = num1 * num2;
+        System.out.println(result);
+        return result;
+    }
+
+
     }
 }
