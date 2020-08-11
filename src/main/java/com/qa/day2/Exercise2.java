9028c4964f8f6b5f518d78e55d48272cdfd6acb1
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-11 09:51:28 +0100

    inputting my 5 variables

--- src/main/java/com/qa/day2/Exercise2.java
+++ src/main/java/com/qa/day2/Exercise2.java
@@ -2,13 +2,17 @@ package com.qa.day2;
 
 public class Exercise2 {
 
+    public static int Physics;
+    public static int Chemistry;
+    public static int Biology;
+    public static int TotalScore;
+    public static int Percentage;
+
     public static void main(String[] args) {
 
     }
 
-    public static string Physics;
-    public static string Chemistry
-    public static string Biology;
+
 
     public static int addingSubjectScores (int sub1, int sub2, int sub3)
 }
