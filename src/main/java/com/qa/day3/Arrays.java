32afa72c428dfec34b1072d1c95ff142ecd2cbc7
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-11 23:16:22 +0100

    creating an array

--- src/main/java/com/qa/day3/Arrays.java
+++ src/main/java/com/qa/day3/Arrays.java
@@ -1,5 +1,9 @@
 package com.qa.day3;
 
 public class Arrays {
-    //public int [] shoeArray
+    public int [] shoeArraySD = new  int[10];
+
+    public static void main(String[] args) {
+        shoeArraySD[0] = 2;
+    }
 }
--- src/main/java/com/qa/day3/Iterations2.java
+++ src/main/java/com/qa/day3/Iterations2.java
@@ -2,5 +2,11 @@ package com.qa.day3;
 
 public class Iterations2 {
 
-    //public static void main(String[] args)
+    public static void main(String[] args) {
+    for(i=100; i<200; i++) {
+        if(i%==0) {
+            System.out.println("-");
+        }
+    }
+    }
 }
