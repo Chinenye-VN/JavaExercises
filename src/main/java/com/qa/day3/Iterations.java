6b8833a94b3dafc1fee52ca505e8d80d8ac304a2
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-11 13:30:27 +0100

    Creating 1st loop

---
+++ src/main/java/com/qa/day3/Iterations.java
@@ -0,0 +1,14 @@
+package com.qa.day3;
+
+public class Iterations {
+
+    public static void main(String[] args) {
+        int A = 100;
+        while(A > 200) {
+            System.out.println(A);
+        A++
+        }
+
+    }
+
+}
