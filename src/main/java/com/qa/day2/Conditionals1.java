d643415a323bc27e0b25715ad21bf23e19661fb8
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-12 10:09:11 +0100

    fixing code

--- src/main/java/com/qa/day2/Conditionals1.java
+++ src/main/java/com/qa/day2/Conditionals1.java
@@ -1,14 +1,20 @@
 package com.qa.day2;
 
+import javax.swing.*;
+
+import static com.qa.day2.Conditionals1.method1;
+
 public class Conditionals1 {
 
-    int a;
-    int b;
-    boolean sum = true;
-
     public static void main(String[] args) {
-        if(sum) {
-            System.out.println();
+        method1(1,2, true);
+    }
+
+    public static int method1 (int a, int b, boolean check) {
+        if (check){
+            System.out.println(a+b);
+        } else {
+            System.out.println(a*b);
         }
     }
 }
--- src/main/java/com/qa/day3/Iterations2.java
+++ src/main/java/com/qa/day3/Iterations2.java
@@ -4,7 +4,7 @@ public class Iterations2 {
 
     public static void main(String[] args) {
     for(i=100; i<200; i++) {
-        if(i%==0) {
+        if(i%=0) {
             System.out.println("-");
         }
     }
