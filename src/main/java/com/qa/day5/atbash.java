a1acfa74ef789ccfd1ae89b0b15d9d9dda468f7a
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-13 17:26:29 +0100

    fixing cipher

---
+++ src/main/java/com/qa/day5/atbash.java
@@ -0,0 +1,54 @@
+//package com.qa.day5;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class atbash {
+    static Scanner scan = new Scanner(System.in);
+    static String  word;
+    public static void main(String[] args) {
+        word = scan.nextLine();
+        encode(word);
+    }
+}
+    public static final String alph = "abcdefghijklmnopqrstuvwxyz";
+
+    public static void encode(String plainText) {
+        //plainText = plainText.toUpperCase();
+        //converting the text to uppercase
+        //String cipherText = "";
+        //initialising empty string to add alphabets
+        //for(int i=0; i< plainText.length(); i++) {
+            //int mappingAlph = alph.indexOf(plainText.charAt(i));
+            // mapping the string to value, e.g A=0,B=1..
+                //}
+
+       // }
+//}
+
+public class atbash{
+    static Scanner scan = new Scanner(System.in);
+    static String  word;
+    public static void main(String[] args) {
+        word = scan.nextLine();
+        convert(word);
+    }
+    public static void convert(String word){
+        String convertedWord = "";
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char[] let = letters.toCharArray();
+        String reverse = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+        char[] rev = reverse.toCharArray();
+
+        for(char str : word.toCharArray()){
+            for(int i = 0; i < let.length; i++){
+                if(let[i] == str){
+                    convertedWord = convertedWord+rev[i];
+                    break;
+                }
+            }
+        }
+        System.out.println(convertedWord);
+    }
+}
