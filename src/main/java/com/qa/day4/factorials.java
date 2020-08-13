45c8f17b7ed05d0c2ed74500ab935fdacbd45980
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-13 15:36:51 +0100

    fixed factorial code

--- src/main/java/com/qa/day4/factorials.java
+++ src/main/java/com/qa/day4/factorials.java
@@ -2,18 +2,23 @@ package com.qa.day4;
 
 public class factorials {
     public static void main(String[] args) {
-        System.out.println(factorial(60));
-
+        factorial(24);
     }
-
-    public static int factorial(int num1) {
+    public static void factorial(int num1) {
         int i = 1;
         while (num1 > 1) {
-            num1 = (num1 / i);
             i++;
+            num1 = (num1 / i);
         }
-        return i-1;
 
+        if (num1 % i != 0) {
+            System.out.println(i++);
+
+        } else {
+            System.out.println("It is not a factorial");
+        }
 
     }
+
 }
+
