36f192c562aa122312742726da7f6e018294dd96
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-14 13:45:20 +0100

    Temperature tutorial test check

---
+++ src/main/java/com/qa/day6/TempConverter.java
@@ -0,0 +1,28 @@
+package com.qa.day6;
+
+public class TempConverter {
+
+    public double convertFahrenheitToCelsius(double fahrenheit) {
+        return (5.0 / 9.0) * (fahrenheit - 32);
+    }
+
+    public float convertCelsiusToFahrenheit(int celsius) {
+        return (9 / 5) * (celsius) + 32;
+    }
+
+    public float convertKelvinToCelsius(int kelvin) {
+        return (kelvin - 273);
+    }
+
+    public float convertCelsiusToKelvin(int celsius) {
+        return (celsius + 273);
+    }
+
+    public float convertKelvinToFahrenheit(int kelvin) {
+        return (9 / 5) * (kelvin - 273) + 32;
+    }
+
+    public float convertFahrenheitToKelvin(int fahrenheit) {
+        return (5 / 9) * (fahrenheit - 32) + 273;
+    }
+}
