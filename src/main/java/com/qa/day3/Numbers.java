e876f47bb83662410419e1398d5230469f8d389f
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-18 19:53:01 +0100

    iterating numbers 1-100

---
+++ src/main/java/com/qa/day3/Numbers.java
@@ -0,0 +1,11 @@
+package com.qa.day3;
+
+public class Numbers {
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=99;i++) {
+            System.out.println(i + ",");
+
+        }
+    }
+}
