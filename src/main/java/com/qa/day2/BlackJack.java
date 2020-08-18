858310d5b85ba1daf89a81d0d0c09a1a1fd90dee
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-18 19:09:54 +0100

    fixed play (1,22) -> 1

--- src/main/java/com/qa/day2/BlackJack.java
+++ src/main/java/com/qa/day2/BlackJack.java
@@ -2,16 +2,25 @@ package com.qa.day2;
 
 public class BlackJack {
     public static void main(String[] args) {
-        blackjack(18,20);
+        blackjack(22,23);
 
     }
     public static int blackjack (int a, int b) {
-        if (a > b && a < 21) {
+        if (a > b && a <= 21 && b <= 21) {
             System.out.println(a);
             return a;
-        } else if (b > a && b < 21) {
+        } else if (a > b && a > 21){
+
+            System.out.println(0);
+            return 0;
+        } else if (b > a && b <= 21 && a <= 21) {
+
             System.out.println(b);
             return b;
+        }else if (b > a && b > 21){
+
+            System.out.println(a);
+            return a;
         } else {
             System.out.println(0);
             return 0;
