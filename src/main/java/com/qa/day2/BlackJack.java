281df96579569b7fe6466f184b6ae0424986e6c0
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-18 19:14:36 +0100

    fixed all cases for different combinations of numbers

--- src/main/java/com/qa/day2/BlackJack.java
+++ src/main/java/com/qa/day2/BlackJack.java
@@ -2,22 +2,22 @@ package com.qa.day2;
 
 public class BlackJack {
     public static void main(String[] args) {
-        blackjack(22,23);
+        blackjack(10,300);
 
     }
     public static int blackjack (int a, int b) {
         if (a > b && a <= 21 && b <= 21) {
             System.out.println(a);
             return a;
-        } else if (a > b && a > 21){
+        } else if (a > b && a > 21 && b < 21){
 
-            System.out.println(0);
-            return 0;
+            System.out.println(b);
+            return b;
         } else if (b > a && b <= 21 && a <= 21) {
 
             System.out.println(b);
             return b;
-        }else if (b > a && b > 21){
+        }else if (b > a && b > 21 && a < 21){
 
             System.out.println(a);
             return a;
