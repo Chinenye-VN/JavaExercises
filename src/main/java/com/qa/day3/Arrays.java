d6bc45dc7c6d79b50f6b757419dd409fc9ee3030
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-18 20:21:36 +0100

    creating an empty array with 10 items

--- src/main/java/com/qa/day3/Arrays.java
+++ src/main/java/com/qa/day3/Arrays.java
@@ -1,11 +1,23 @@
 package com.qa.day3;
 
+
 public class Arrays {
 
     public static int[] ageArraySD = new int[10];
 
+
     public static void main(String[] args) {
 
         ageArraySD[0] = 2;
+        ageArraySD[1] = 3;
+        ageArraySD[2] = 34;
+        ageArraySD[3] = 33;
+        ageArraySD[4] = 23;
+        ageArraySD[5] = 53;
+        ageArraySD[6] = 36;
+        ageArraySD[7] = 35;
+        ageArraySD[8] = 13;
+        ageArraySD[9] = 4;
+        System.out.println(ageArraySD[2]);
     }
 }
