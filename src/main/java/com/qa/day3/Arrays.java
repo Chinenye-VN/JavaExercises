cbebe59411ad1734278b84b51f5a23fecd0ccfe1
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-18 20:25:39 +0100

    iterating loops to output items in an array list

--- src/main/java/com/qa/day3/Arrays.java
+++ src/main/java/com/qa/day3/Arrays.java
@@ -3,21 +3,10 @@ package com.qa.day3;
 
 public class Arrays {
 
-    public static int[] ageArraySD = new int[10];
-
-
+    public static int[] objects = {1,2,3,4,5,6,7,8,9,10};
     public static void main(String[] args) {
-
-        ageArraySD[0] = 2;
-        ageArraySD[1] = 3;
-        ageArraySD[2] = 34;
-        ageArraySD[3] = 33;
-        ageArraySD[4] = 23;
-        ageArraySD[5] = 53;
-        ageArraySD[6] = 36;
-        ageArraySD[7] = 35;
-        ageArraySD[8] = 13;
-        ageArraySD[9] = 4;
-        System.out.println(ageArraySD[2]);
+        for (int i = 0; i < objects.length; i++) {
+            System.out.println(objects[i]);
+        }
     }
 }
